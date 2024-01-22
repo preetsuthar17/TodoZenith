b072ccd074772e2fe50f169669e9ffdd1afa5145
Preet Suthar <preetsutharxd@gmail.com>
2024-01-22 11:33:53 +0530

    remove: reminder system

--- src/scripts/service-worker.js
+++
@@ -1,9 +0,0 @@
-// // service-worker.js
-
-// self.addEventListener("push", (event) => {
-//   const options = {
-//     body: event.data.text(),
-//   };
-
-//   event.waitUntil(self.registration.showNotification("Ding dong! 🔔", options));
-// });
